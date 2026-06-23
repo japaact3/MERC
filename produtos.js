20836857fab7789eb6a619155c267dfd1c624e00
japaact3 <127565484+japaact3@users.noreply.github.com>
2026-06-22 23:59:28 -0300

    versão1.7

--- produtos.js
+++ produtos.js
@@ -1,3 +1,5 @@
+const imgtst = "img/carrinhoteste.jpg"
+
 const produtos = [
 
     {
@@ -5,7 +7,7 @@ const produtos = [
         nome: "Arroz Tio João 5kg",
         preco: 29.90,
         categoria: "Mercearia",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 20,
         destaque: true
     },
@@ -15,7 +17,7 @@ const produtos = [
         nome: "Feijão Carioca 1kg",
         preco: 8.99,
         categoria: "Mercearia",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 15,
         destaque: false
     },
@@ -25,7 +27,7 @@ const produtos = [
         nome: "Macarrão Espaguete",
         preco: 5.49,
         categoria: "Mercearia",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 30,
         destaque: false
     },
@@ -35,7 +37,7 @@ const produtos = [
         nome: "Coca-Cola 2L",
         preco: 12.99,
         categoria: "Bebidas",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 25,
         destaque: true
     },
@@ -45,7 +47,7 @@ const produtos = [
         nome: "Guaraná Antarctica 2L",
         preco: 10.99,
         categoria: "Bebidas",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 18,
         destaque: false
     },
@@ -55,7 +57,7 @@ const produtos = [
         nome: "Água Mineral 500ml",
         preco: 2.50,
         categoria: "Bebidas",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 50,
         destaque: false
     },
@@ -65,7 +67,7 @@ const produtos = [
         nome: "Banana Prata",
         preco: 6.99,
         categoria: "Hortifruti",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 40,
         destaque: false
     },
@@ -75,7 +77,7 @@ const produtos = [
         nome: "Maçã Gala",
         preco: 8.99,
         categoria: "Hortifruti",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 35,
         destaque: false
     },
@@ -85,7 +87,7 @@ const produtos = [
         nome: "Tomate",
         preco: 7.99,
         categoria: "Hortifruti",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 22,
         destaque: false
     },
@@ -95,7 +97,7 @@ const produtos = [
         nome: "Detergente Ypê",
         preco: 2.99,
         categoria: "Limpeza",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 20,
         destaque: false
     },
@@ -105,7 +107,7 @@ const produtos = [
         nome: "Sabão em Pó OMO",
         preco: 18.99,
         categoria: "Limpeza",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 12,
         destaque: true
     },
@@ -115,7 +117,7 @@ const produtos = [
         nome: "Água Sanitária",
         preco: 4.99,
         categoria: "Limpeza",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 15,
         destaque: false
     },
@@ -125,7 +127,7 @@ const produtos = [
         nome: "Shampoo Seda",
         preco: 12.90,
         categoria: "Higiene",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 20,
         destaque: false
     },
@@ -135,7 +137,7 @@ const produtos = [
         nome: "Sabonete Dove",
         preco: 3.99,
         categoria: "Higiene",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 25,
         destaque: false
     },
@@ -145,7 +147,7 @@ const produtos = [
         nome: "Creme Dental Colgate",
         preco: 5.99,
         categoria: "Higiene",
-        imagem: "img/carrinhoteste.jfif",
+        imagem: imgtst,
         estoque: 18,
         destaque: false
     }
